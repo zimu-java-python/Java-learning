6cdc62b7fc924b7ead11879b3ca0b9f9f191c4a9
zimu-java-python <2869758975@qq.com>
2026-03-08 20:02:44 +0800

    这是第一个Java程序，HelloWorld

---
+++ day01-JavaBasic/src/com/java/learning/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.java.learning;
+
+public class HelloWorld {
+    // 这是我的第一个Java程序，HelloWorld
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
