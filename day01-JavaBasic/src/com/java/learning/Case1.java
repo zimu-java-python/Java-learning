0e4ff75f8b1f039cad997b1ec30768735b305f2c
zimu-java-python <2869758975@qq.com>
2026-03-09 22:05:32 +0800

    day01-练习案例

---
+++ day01-JavaBasic/src/com/java/learning/Case1.java
@@ -0,0 +1,11 @@
+package com.java.learning;
+
+public class Case1 {
+    // 案例一：个人信息输出
+    public static void main(String[] args) {
+        System.out.println("姓名：admin");
+        System.out.println();
+        System.out.println("性别：男");
+        System.out.println("住址：电子科技大学中山学院");
+    }
+}
---
+++ day01-JavaBasic/src/com/java/learning/Case2.java
@@ -0,0 +1,10 @@
+package com.java.learning;
+
+public class Case2 {
+    // 输出心形（用换行符，制表符和空格）
+    public static void main(String[] args) {
+        System.out.println("*\t*");
+        System.out.println("*\n*");
+
+    }
+}
