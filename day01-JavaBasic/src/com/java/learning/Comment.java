76e1d16fed22d9839186dc9f536a4b84a0a77a7e
zimu-java-python <2869758975@qq.com>
2026-03-09 21:56:58 +0800

    解释文档注释

---
+++ day01-JavaBasic/src/com/java/learning/Comment.java
@@ -0,0 +1,20 @@
+package com.java.learning;
+/*
+* 文档注释：
+* 文档注释可以被JDK提供的工具 javadoc 所解析，生成一套以网页文件形式体现的该程序的说明文档
+* */
+
+/**
+ * 这是我的第一个Java程序
+ * @author admin
+ * @Version 1.0
+ *
+ */
+public class Comment {
+    /**
+     * 这是main方法，格式是固定的
+     * */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
