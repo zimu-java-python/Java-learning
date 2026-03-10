e240b5fd7a2f5730dd115fb857a1ef68b8919000
zimu-java-python <2869758975@qq.com>
2026-03-10 20:23:18 +0800

    day02-java中的关键字

---
+++ day02-JavaBasic/src/com/java/learning/Keyword.java
@@ -0,0 +1,15 @@
+package com.java.learning;
+// java中的关键字
+/*
+* 定义：被Java语言赋予了特殊含义，用于专门用途的字符串或单词
+* HelloWorld案例中出现的关键字有：class、public、static、void等
+* 特点：关键字都是“小写字母”
+* 其中：const和goto是保留字，也不可以使用
+* true、false，null不在其中，看起来像关键字，其实是字面量
+* */
+
+public class Keyword {
+    public static void main(String[] args) {
+        System.out.println("keyword");
+    }
+}
