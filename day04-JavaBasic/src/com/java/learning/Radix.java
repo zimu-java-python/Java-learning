7bb75d835f841a7a341e22a473568fabee7aaa77
zimu-java-python <2869758975@qq.com>
2026-03-12 20:41:25 +0800

    day04-关于计算机中的进制

---
+++ day04-JavaBasic/src/com/java/learning/Radix.java
@@ -0,0 +1,14 @@
+package com.java.learning;
+// 演示进制
+public class Radix {
+    public static void main(String[] args) {
+        int num1 = 103;// 十进制
+        int num2 = 0b10;// 二进制:0b开头
+        int num3 = 023;// 八进制:0开头
+        int num4 = 0X23a;// 十六进制:0X开头
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
