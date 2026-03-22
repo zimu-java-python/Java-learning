2e5f8d9cec5b57dc7212754cceed6e7ddcc6b596
zimu-java-python <2869758975@qq.com>
2026-03-22 20:35:14 +0800

    day14-阶段一项目：谷粒记账软件的实现（后续再做）

---
+++ Weektwo/day14-JavaBasic/src/com/java/learning/GuLiAccount.java
@@ -0,0 +1,19 @@
+package com.java.learning;
+// 阶段一项目：谷粒记账软件的实现
+
+// 后续再做
+public class GuLiAccount {
+    public static void main(String[] args) {
+
+        while(true){
+
+            System.out.println("-----------------谷粒记账软件-----------------\n");
+            System.out.println("                 1 收支明细");
+            System.out.println("                 2 登记收入");
+            System.out.println("                 3 登记支出");
+            System.out.println("                 4 退    出\n");
+            System.out.print("                 请选择(1-4)：");
+
+        }
+    }
+}
