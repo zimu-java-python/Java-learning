48815c12779ceec69f95a866ffcb5a5fa3e5fedb
zimu-java-python <2869758975@qq.com>
2026-03-23 20:31:16 +0800

    测试

---
+++ Week3/day15-JavaBasic/src/com/java/learning/HelloChina.java
@@ -0,0 +1,12 @@
+package com.java.learning;
+
+/**
+ * ClassName：HelloChina
+ * Description：
+ *这是一个测试程序
+ * @Author 刁博翔
+ * @Create 2026/3/23 20:26
+ * @Version 1.0
+ */
+public class HelloChina {
+}
---
+++ Week3/day15-JavaBasic/src/com/java/learning/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.java.learning;
+/*
+对idea进行优化设置
+ */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        Map map = new HashMap();
+    }
+}
