290737e46c953d451e6f9fb142ccb84fe119c1e1
zimu-java-python <2869758975@qq.com>
2026-03-29 20:38:22 +0800

    测试

--- Week3/day19-JavaBasic/src/com/java/learning/Text.java
+++ Week3/day19-JavaBasic/src/com/java/learning/Text.java
@@ -19,9 +19,9 @@ public class Text {
         System.out.println();
 
         // 冒泡排序
-        for(int i=0;i<arr.length - 1;i++){
-            for(int j=0;j<arr.length-1-i;j++){
-                if(arr[j]>arr[j+1]){
+        for (int i = 0;i < arr.length - 1;i++){
+            for (int j = 0;j < arr.length- 1 - i;j++){
+                if (arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
