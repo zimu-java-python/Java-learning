9fe789b2c93ab94dfd0b14d8a0745f8ccdf09820
zimu-java-python <2869758975@qq.com>
2026-03-31 20:43:45 +0800

    类的成员之一：属性，案例一

---
+++ Week4/day23-JavaBasic/src/com/java/learning1/Employee.java
@@ -0,0 +1,18 @@
+package com.java.learning1;
+
+/**
+ * ClassName：Employee
+ * Description：
+ *
+ * @Author 刁博翔
+ * @Create 2026/3/31 20:20
+ * @Version 1.0
+ */
+public class Employee {
+
+    // 属性
+    int id;
+    String name;
+    int age;
+    double salary;
+}
