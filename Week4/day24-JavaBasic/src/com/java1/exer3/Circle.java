eab4c226baa42807fea14083dc253aeb9d30deb1
zimu-java-python <2869758975@qq.com>
2026-04-01 20:24:58 +0800

    类的成员之一：方法

---
+++ Week4/day24-JavaBasic/src/com/java1/exer3/Circle.java
@@ -0,0 +1,19 @@
+package com.java1.exer3;
+
+/**
+ * ClassName：Circle
+ * Description：
+ *
+ * @Author 刁博翔
+ * @Create 2026/4/1 20:16
+ * @Version 1.0
+ */
+public class Circle {
+    // 属性
+    double radius;// 半径
+
+    // 方法
+    public void findArea(){
+        System.out.println("面积为" + (3.14 * radius * radius));
+    }
+}
---
+++ Week4/day24-JavaBasic/src/com/java1/exer3/CircleTest.java
@@ -0,0 +1,17 @@
+package com.java1.exer3;
+
+/**
+ * ClassName：CircleTest
+ * Description：
+ *
+ * @Author 刁博翔
+ * @Create 2026/4/1 20:19
+ * @Version 1.0
+ */
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle  c1 = new Circle();
+        c1.radius = 2.3;
+        c1.findArea();
+    }
+}
